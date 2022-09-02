ee649b7b65b83f5198c4bbe1ed71f6b0c8b995f1
Eric Adam <eadamfs.programador@gmail.com>
2022-09-01 21:58:23 -0300

    criando conexao com o banco de dados

--- crudPython.py
+++ crudPython.py
@@ -1,1 +1,15 @@
 import mysql.connector
+
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='dbaulacrud'
+)
+
+cursor = conexao.cursor()
+
+# CRUD
+
+cursor.close()
+conexao.close()
